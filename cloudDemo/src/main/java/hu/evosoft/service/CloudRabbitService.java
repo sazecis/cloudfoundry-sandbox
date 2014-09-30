e69d35cde7bde19b03afef43ed99cb0bba345046
Csaba Szegedi <szegedi.csaba.81@gmail.com>
2014-09-30 17:17:14 +0200

    Started to work on Rabbit message listener

--- cloudDemo/src/main/java/hu/evosoft/service/CloudRabbitService.java
+++ cloudDemo/src/main/java/hu/evosoft/service/CloudRabbitService.java
@@ -1,8 +1,11 @@
 package hu.evosoft.service;
 
 import hu.evosoft.model.Data;
+import hu.evosoft.model.LogFile;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -10,10 +13,16 @@ import org.springframework.stereotype.Repository;
 public class CloudRabbitService {
 
 	private static final String QUEUE_NAME = "cloudDemo"; 
-	
-    @Autowired 
+
+	@Autowired 
     private RabbitTemplate rabbitTemplate;
-       
+    @Autowired
+    SimpleMessageListenerContainer rabbitListenerContainer;
+	
+	public CloudRabbitService() {
+		rabbitListenerContainer.setMessageListener(new MessageListenerAdapter(LogFile.class));
+	}
+	
     public void queueMessage(String name) {
     	rabbitTemplate.setQueue(QUEUE_NAME);
         rabbitTemplate.convertAndSend(QUEUE_NAME, name);
